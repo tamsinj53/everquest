d0955de552853be734fa7754c0e270c7688d8505
Tamsin Powell <Tamsin.Powell@proton.me>
2023-08-27 16:13:25 +0100

    sheet func

--- inventory.py
+++ inventory.py
@@ -7,7 +7,24 @@ import glob
 import gspread as gs
 import pandas as pd
 
+import pygsheets
 
+def write_to_gsheet(service_file_path, spreadsheet_id, sheet_name, data_df):
+    """
+    this function takes data_df and writes it under spreadsheet_id
+    and sheet_name using your credentials under service_file_path
+    """
+    gc = pygsheets.authorize(service_file=service_file_path)
+    sh = gc.open_by_key(spreadsheet_id)
+    try:
+        sh.add_worksheet(sheet_name)
+    except:
+        pass
+    wks_write = sh.worksheet_by_title(sheet_name)
+    wks_write.clear('A1',None,'*')
+    wks_write.set_dataframe(data_df, (1,1), encoding='utf-8', fit=True)
+    wks_write.frozen_rows = 1
+    
 def read_file(filename):
     rows = []
     with open(filename,'r') as fd:
